358fdd913e1b9e8c10ceb69d1394b591940507a6
Ludger Radke <113907719+ludgerradke@users.noreply.github.com>
2023-09-25 12:44:57 +0200

    change phantom size to 128

--- CEST_Generator_2D/main.m
+++ CEST_Generator_2D/main.m
@@ -9,7 +9,7 @@
 % and analysis of synthetic data in CEST imaging studies, enabling detailed examination of 
 % image processing and analysis techniques.
 %%
-% clc;
+clc;
 clear all;
 close all;
 rng('shuffle');
@@ -36,7 +36,7 @@ for k = 1:numIterations
     Scanner_parameter.n = randi([8,20]);             % Number of excitations
     Scanner_parameter.ppm_range = randi([3,6]);      % Chemical shift range in ppm
     
-    [A, B, C, D, E, mask] = generate_pool_params(20); % Generate pool parameters
+    [A, B, C, D, E, mask] = generate_pool_params(128); % Generate pool parameters
     dyn = 100; % Assuming dyn is a constant, you need to specify its value
     image = zeros(size(A.R1,1), size(A.R1,2), 1, dyn);
     
@@ -47,19 +47,9 @@ for k = 1:numIterations
         for i = 1:numel(x)
             pixel_CEST_Parameter = update_pixel_params(CEST_Parameter, A, B, C, D, E, x(i), y(i));
             
-            % Updating pool parameters for each pixel
-            [A, B, C, D, E] = update_pools(A, B, C, D, E, pixel_CEST_Parameter, x(i), y(i));
-            
             [Z, ppm] = generate_Z_spectrum(Scanner_parameter, pixel_CEST_Parameter, dyn);
             image(x(i), y(i), 1, :) = Z;
         end
-    end
-    
-    % Save data for each iteration
-    if k < 100
-        iteration_folder = fullfile(root_data_folder, num2str(k));
-        mkdir(iteration_folder);
-        save_maps(A, B, C, D, E, mask, iteration_folder);
     end
     
     niftiwrite(int16(image * 4016), fullfile(root_data_folder, [num2str(k) '.nii']), 'Compressed', false);
